bc7fb9411d3d30b9327fdee5bbefd4e6720c5e79
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2020-09-25 11:51:32 +0100

    Added pagination

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const apikey = config.apikey;
 const imgPath = `http://image.tmdb.org/t/p/w300`;
+let currentPage = 1;
 
 const latestMovies = document.querySelector('#latest');
 const searchedMovies = document.querySelector('#searched')
 const form = document.querySelector('#form');
 const userInput = document.querySelector('#userInput')
-const searchedHeader = document.querySelector('.movie-searched__header')
+const searchedHeader = document.querySelector('.movie-searched__header');
+const nextPage = document.querySelector('#next');
+const prevPage = document.querySelector('#prev')
 
-form.addEventListener('submit', getSearchedMovies)
+function eventListeners() {
+  form.addEventListener('submit', getSearchedMovies)
+  nextPage.addEventListener('click', switchPage);
+  prevPage.addEventListener('click', switchPage);
+}
 
 function parseLatest(results) {
   searchedHeader.style.display = 'none';
@@ -37,15 +44,23 @@ function parseSearched(results) {
   return parsed;
 }
 
+function switchPage(e) {
+  e.preventDefault();
+  e.target.id === nextPage.id ?
+    (currentPage = currentPage + 1) :
+    (currentPage = currentPage - 1)
+  getLatestMovies(currentPage)
+}
+
 function getLatestMovies() {
-  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false`)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&page=${currentPage}&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false`)
     .then(response => response.json())
     .then(data => latestMovies.innerHTML = parseLatest(data.results))
     .catch(error => console.log(error))
 }
 
 function getSearchedMovies(e) {
-  e.preventDefault(e);
+  e.preventDefault();
   const searchContent = userInput.value;
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${searchContent}`)
     .then(response => response.json())
@@ -54,7 +69,7 @@ function getSearchedMovies(e) {
 }
 
 getLatestMovies()
-
+eventListeners();
 
 
 
